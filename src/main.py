a9687c45a61b0a02b7535607e5b7b7867f1ace9b
DmitriIli <di.grebenev@outlook.com>
2025-03-26 06:05:49 +0300

    add

--- src/main.py
+++ src/main.py
@@ -1,9 +1,21 @@
 import asyncio
 import sys
 import os
-from queries.core import async_connection, create_tables
+from queries.core import async_connection, create_tables, insert_date
 
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-asyncio.run(create_tables())
+# asyncio.run(create_tables())
+# asyncio.run(insert_date())
+
+
+
+async def main():
+    if __name__ == '__main__':
+        task1 = asyncio.create_task(create_tables())
+        task2 = asyncio.create_task(insert_date())
+        await task1
+        await task2
+
+asyncio.run(main())
--- src/queries/core.py
+++ src/queries/core.py
@@ -1,6 +1,9 @@
+import asyncio
+
 from sqlalchemy import text
 from database import engine
 from models import metadata_obj
+
 
 
 async def async_connection():
@@ -12,5 +15,17 @@ async def async_connection():
 
 async def create_tables():
     async with engine.begin() as conn:
+        engine.echo = False
         await conn.run_sync(metadata_obj.drop_all)
         await conn.run_sync(metadata_obj.create_all)
+        engine.echo = True
+
+
+async def insert_date():
+    async with engine.begin() as conn:
+        await asyncio.sleep(1)
+        stmt = """ INSERT INTO workers(user_name) VALUES
+                ('Jonh'),
+                ('Karl'); """
+        await conn.execute(text(stmt))
+        await conn.commit() 
